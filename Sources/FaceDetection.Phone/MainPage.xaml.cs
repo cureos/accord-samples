34003cd2082bd98501f402ed50947fa1943a81ec
Anders Gustafsson <anders@cureos.com>
2015-01-29 08:53:19 +0100

    Use NuGet to obtain AForge and Accord packages

--- Sources/FaceDetection.Phone/MainPage.xaml.cs
+++ Sources/FaceDetection.Phone/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) 2010-2015 Anders Gustafsson, Cureos AB.
+// All rights reserved. 
+
+using System;
 using System.Collections;
 using System.Drawing;
 using System.Windows;
@@ -8,58 +11,66 @@ using Accord.Vision.Detection;
 
 namespace FaceDetection
 {
-	public partial class MainPage
-	{
-		private Bitmap picture;
-		private HaarObjectDetector detector;
+    using System.Windows.Media;
 
-		// Constructor
-		public MainPage()
-		{
-			InitializeComponent();
-		}
+    using Color = System.Drawing.Color;
 
-		public static ICollection SearchModes
-		{
-			get { return Enum.GetValues(typeof (ObjectDetectorSearchMode)); }
-		}
+    public partial class MainPage
+    {
+        private Bitmap picture;
+        private HaarObjectDetector detector;
 
-		public static ICollection ScalingModes
-		{
-			get { return Enum.GetValues(typeof (ObjectDetectorScalingMode)); }
-		}
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
 
-		private void MainPage_OnLoaded(object sender, RoutedEventArgs e)
-		{
-			var bmpi = new BitmapImage();
-			bmpi.SetSource(Application.GetResourceStream(new Uri(@"Resources/judybats.jpg", UriKind.Relative)).Stream);
-			picture = new WriteableBitmap(bmpi);
-			pictureBox1.Source = picture;
+        public static ICollection SearchModes
+        {
+            get { return Enum.GetValues(typeof (ObjectDetectorSearchMode)); }
+        }
 
-			cbMode.SelectedItem = ObjectDetectorSearchMode.NoOverlap;
-			cbScaling.SelectedItem = ObjectDetectorScalingMode.SmallerToGreater;
+        public static ICollection ScalingModes
+        {
+            get { return Enum.GetValues(typeof (ObjectDetectorScalingMode)); }
+        }
 
-			HaarCascade cascade =
-				HaarCascade.FromXml(
-					Application.GetResourceStream(new Uri(@"Resources/haarcascade_frontalface_alt.xml", UriKind.Relative)).Stream);
-			detector = new HaarObjectDetector(cascade, 30);
-		}
+        private void MainPage_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var bmpi = new BitmapImage();
+            bmpi.SetSource(Application.GetResourceStream(new Uri(@"Resources/judybats.jpg", UriKind.Relative)).Stream);
+            pictureBox1.Source = new WriteableBitmap(bmpi);
 
-		private void Button1_OnClick(object sender, RoutedEventArgs e)
-		{
-			detector.SearchMode = (ObjectDetectorSearchMode)cbMode.SelectedItem;
-			detector.ScalingMode = (ObjectDetectorScalingMode)cbScaling.SelectedItem;
-			detector.ScalingFactor = 1.5f;
-			detector.UseParallelProcessing = false;
+            // Explicit cast from ImageSource to System.Drawing.Bitmap
+            picture = (Bitmap)pictureBox1.Source;
 
-			// Process frame to detect objects
-			Rectangle[] objects = detector.ProcessFrame(picture);
+            cbMode.SelectedItem = ObjectDetectorSearchMode.NoOverlap;
+            cbScaling.SelectedItem = ObjectDetectorScalingMode.SmallerToGreater;
 
-			if (objects.Length > 0)
-			{
-				RectanglesMarker marker = new RectanglesMarker(objects, Color.FromArgb(0xff, 0xff, 0x00, 0xff));
-				pictureBox1.Source = marker.Apply(picture);
-			}
-		}
-	}
+            HaarCascade cascade =
+                HaarCascade.FromXml(
+                    Application.GetResourceStream(new Uri(@"Resources/haarcascade_frontalface_alt.xml", UriKind.Relative)).Stream);
+            detector = new HaarObjectDetector(cascade, 30);
+        }
+
+        private void Button1_OnClick(object sender, RoutedEventArgs e)
+        {
+            detector.SearchMode = (ObjectDetectorSearchMode)cbMode.SelectedItem;
+            detector.ScalingMode = (ObjectDetectorScalingMode)cbScaling.SelectedItem;
+            detector.ScalingFactor = 1.5f;
+            detector.UseParallelProcessing = false;
+
+            // Process frame to detect objects
+            var objects = detector.ProcessFrame(picture);
+
+            if (objects.Length > 0)
+            {
+                var marker = new RectanglesMarker(objects, Color.FromArgb(0xff, 0xff, 0x00, 0xff));
+
+                // Explicit cast from System.Drawing.Bitmap to ImageSource
+                pictureBox1.Source = (ImageSource)marker.Apply(picture);
+            }
+        }
+    }
 }
--- Sources/Panorama.Phone/MainPage.xaml.cs
+++ Sources/Panorama.Phone/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) 2010-2015 Anders Gustafsson, Cureos AB.
+// All rights reserved.
+
+using System;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Windows;
@@ -10,115 +13,117 @@ using AForge;
 
 namespace Panorama
 {
-	public partial class MainPage
-	{
-		private Bitmap img1;
-		private Bitmap img2;
+    using System.Windows.Media;
 
-		private IntPoint[] harrisPoints1;
-		private IntPoint[] harrisPoints2;
+    public partial class MainPage
+    {
+        private Bitmap img1;
+        private Bitmap img2;
 
-		private IntPoint[] correlationPoints1;
-		private IntPoint[] correlationPoints2;
+        private IntPoint[] harrisPoints1;
+        private IntPoint[] harrisPoints2;
 
-		private MatrixH homography;
+        private IntPoint[] correlationPoints1;
+        private IntPoint[] correlationPoints2;
 
-		// Constructor
-		public MainPage()
-		{
-			InitializeComponent();
-		}
+        private MatrixH homography;
 
-		private void MainPage_OnLoaded(object sender, RoutedEventArgs e)
-		{
-			var bmpi = new BitmapImage();
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
 
-			bmpi.SetSource(Application.GetResourceStream(new Uri(@"Resources/UFSCar_Lake1.png", UriKind.Relative)).Stream);
-			img1 = ((Bitmap)new WriteableBitmap(bmpi)).Clone(PixelFormat.Format24bppRgb);
+        private void MainPage_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var bmpi = new BitmapImage();
 
-			bmpi.SetSource(Application.GetResourceStream(new Uri(@"Resources/UFSCar_Lake2.png", UriKind.Relative)).Stream);
-			img2 = ((Bitmap)new WriteableBitmap(bmpi)).Clone(PixelFormat.Format24bppRgb);
+            bmpi.SetSource(Application.GetResourceStream(new Uri(@"Resources/UFSCar_Lake1.png", UriKind.Relative)).Stream);
+            img1 = ((Bitmap)new WriteableBitmap(bmpi)).Clone(PixelFormat.Format24bppRgb);
 
-			// Concatenate and show entire image at start
-			Concatenate concatenate = new Concatenate(img1);
-			PictureBox.Source = concatenate.Apply(img2);
-		}
+            bmpi.SetSource(Application.GetResourceStream(new Uri(@"Resources/UFSCar_Lake2.png", UriKind.Relative)).Stream);
+            img2 = ((Bitmap)new WriteableBitmap(bmpi)).Clone(PixelFormat.Format24bppRgb);
 
-		private void BtnHarris_OnClick(object sender, RoutedEventArgs e)
-		{
-			// Step 1: Detect feature points using Harris Corners Detector
-			HarrisCornersDetector harris = new HarrisCornersDetector(0.04f, 1000f);
-			harrisPoints1 = harris.ProcessImage(img1).ToArray();
-			harrisPoints2 = harris.ProcessImage(img2).ToArray();
+            // Concatenate and show entire image at start
+            var concatenate = new Concatenate(img1);
+            PictureBox.Source = (ImageSource)concatenate.Apply(img2);
+        }
 
-			// Show the marked points in the original images
-			Bitmap img1mark = new PointsMarker(harrisPoints1).Apply(img1);
-			Bitmap img2mark = new PointsMarker(harrisPoints2).Apply(img2);
+        private void BtnHarris_OnClick(object sender, RoutedEventArgs e)
+        {
+            // Step 1: Detect feature points using Harris Corners Detector
+            var harris = new HarrisCornersDetector(0.04f, 1000f);
+            harrisPoints1 = harris.ProcessImage(img1).ToArray();
+            harrisPoints2 = harris.ProcessImage(img2).ToArray();
 
-			// Concatenate the two images together in a single image (just to show on screen)
-			Concatenate concatenate = new Concatenate(img1mark);
-			PictureBox.Source = concatenate.Apply(img2mark);
-		}
+            // Show the marked points in the original images
+            var img1mark = new PointsMarker(harrisPoints1).Apply(img1);
+            var img2mark = new PointsMarker(harrisPoints2).Apply(img2);
 
-		private void BtnCorrelation_OnClick(object sender, RoutedEventArgs e)
-		{
-			// Step 2: Match feature points using a correlation measure
-			CorrelationMatching matcher = new CorrelationMatching(9);
-			IntPoint[][] matches = matcher.Match(img1, img2, harrisPoints1, harrisPoints2);
+            // Concatenate the two images together in a single image (just to show on screen)
+            var concatenate = new Concatenate(img1mark);
+            PictureBox.Source = (ImageSource)concatenate.Apply(img2mark);
+        }
 
-			// Get the two sets of points
-			correlationPoints1 = matches[0];
-			correlationPoints2 = matches[1];
+        private void BtnCorrelation_OnClick(object sender, RoutedEventArgs e)
+        {
+            // Step 2: Match feature points using a correlation measure
+            var matcher = new CorrelationMatching(9, img1, img2);
+            var matches = matcher.Match(harrisPoints1, harrisPoints2);
 
-			// Concatenate the two images in a single image (just to show on screen)
-			Concatenate concat = new Concatenate(img1);
-			Bitmap img3 = concat.Apply(img2);
+            // Get the two sets of points
+            correlationPoints1 = matches[0];
+            correlationPoints2 = matches[1];
 
-			// Show the marked correlations in the concatenated image
-			PairsMarker pairs = new PairsMarker(
-				correlationPoints1, // Add image1's width to the X points to show the markings correctly
-				correlationPoints2.Apply(p => new IntPoint(p.X + img1.Width, p.Y)));
+            // Concatenate the two images in a single image (just to show on screen)
+            var concat = new Concatenate(img1);
+            var img3 = concat.Apply(img2);
 
-			PictureBox.Source = pairs.Apply(img3);
-		}
+            // Show the marked correlations in the concatenated image
+            var pairs = new PairsMarker(
+                correlationPoints1, // Add image1's width to the X points to show the markings correctly
+                correlationPoints2.Apply(p => new IntPoint(p.X + img1.Width, p.Y)));
 
-		private void BtnRansac_OnClick(object sender, RoutedEventArgs e)
-		{
-			// Step 3: Create the homography matrix using a robust estimator
-			RansacHomographyEstimator ransac = new RansacHomographyEstimator(0.001, 0.99);
-			homography = ransac.Estimate(correlationPoints1, correlationPoints2);
+            PictureBox.Source = (ImageSource)pairs.Apply(img3);
+        }
 
-			// Plot RANSAC results against correlation results
-			IntPoint[] inliers1 = correlationPoints1.Submatrix(ransac.Inliers);
-			IntPoint[] inliers2 = correlationPoints2.Submatrix(ransac.Inliers);
+        private void BtnRansac_OnClick(object sender, RoutedEventArgs e)
+        {
+            // Step 3: Create the homography matrix using a robust estimator
+            var ransac = new RansacHomographyEstimator(0.001, 0.99);
+            homography = ransac.Estimate(correlationPoints1, correlationPoints2);
 
-			// Concatenate the two images in a single image (just to show on screen)
-			Concatenate concat = new Concatenate(img1);
-			Bitmap img3 = concat.Apply(img2);
+            // Plot RANSAC results against correlation results
+            var inliers1 = correlationPoints1.Submatrix(ransac.Inliers);
+            var inliers2 = correlationPoints2.Submatrix(ransac.Inliers);
 
-			// Show the marked correlations in the concatenated image
-			PairsMarker pairs = new PairsMarker(
-				inliers1, // Add image1's width to the X points to show the markings correctly
-				inliers2.Apply(p => new IntPoint(p.X + img1.Width, p.Y)));
+            // Concatenate the two images in a single image (just to show on screen)
+            var concat = new Concatenate(img1);
+            var img3 = concat.Apply(img2);
 
-			PictureBox.Source = pairs.Apply(img3);
-		}
+            // Show the marked correlations in the concatenated image
+            var pairs = new PairsMarker(
+                inliers1, // Add image1's width to the X points to show the markings correctly
+                inliers2.Apply(p => new IntPoint(p.X + img1.Width, p.Y)));
 
-		private void BtnBlend_OnClick(object sender, RoutedEventArgs e)
-		{
-			// Step 4: Project and blend the second image using the homography
-			Blend blend = new Blend(homography, img1);
-			PictureBox.Source = blend.Apply(img2);
-		}
+            PictureBox.Source = (ImageSource)pairs.Apply(img3);
+        }
 
-		private void BtnDoItAll_OnClick(object sender, RoutedEventArgs e)
-		{
-			// Do it all
-			BtnHarris_OnClick(sender, e);
-			BtnCorrelation_OnClick(sender, e);
-			BtnRansac_OnClick(sender, e);
-			BtnBlend_OnClick(sender, e);
+        private void BtnBlend_OnClick(object sender, RoutedEventArgs e)
+        {
+            // Step 4: Project and blend the second image using the homography
+            var blend = new Blend(homography, img1);
+            PictureBox.Source = (ImageSource)blend.Apply(img2);
+        }
 
-		}
-	}
+        private void BtnDoItAll_OnClick(object sender, RoutedEventArgs e)
+        {
+            // Do it all
+            BtnHarris_OnClick(sender, e);
+            BtnCorrelation_OnClick(sender, e);
+            BtnRansac_OnClick(sender, e);
+            BtnBlend_OnClick(sender, e);
+
+        }
+    }
 }
