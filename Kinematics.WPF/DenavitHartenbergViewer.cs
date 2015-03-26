a074eed687833ec795438c8850772e3260df3b68
Anders Gustafsson <anders@cureos.com>
2015-03-26 11:11:08 +0100

    Added kinematic chain sample for WPF

---
+++ Kinematics.WPF/DenavitHartenbergViewer.cs
@@ -0,0 +1,389 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="" file="DenavitHartenbergViewer.cs">
+//   
+// </copyright>
+// <summary>
+//   
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kinematics.WPF
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+
+    using Accord.Math.Kinematics;
+
+    using AForge.Math;
+
+    using Color = System.Windows.Media.Color;
+    using FontFamily = System.Windows.Media.FontFamily;
+    using Point = System.Windows.Point;
+
+    /// <summary>
+    ///   Denavit Hartenberg Viewer.
+    /// </summary>
+    /// <remarks>
+    ///   This class can be used to visualize a D-H model as bitmaps.
+    /// </remarks>
+    /// 
+    public class DenavitHartenbergViewer
+    {
+        // Bitmaps (one for XY, one for YZ and one for XZ planes)
+        /// <summary>
+        /// </summary>
+        WriteableBitmap xy;
+
+        /// <summary>
+        /// </summary>
+        WriteableBitmap yz;
+
+        /// <summary>
+        /// </summary>
+        WriteableBitmap xz;
+
+        /// <summary>
+        ///   Gets or sets the color of the links between joints
+        /// </summary>
+        /// 
+        public Color LinksColor { get; set; }
+
+        /// <summary>
+        ///   Gets or sets the color of the joints
+        /// </summary>
+        /// 
+        public Color JointsColor { get; set; }
+
+        /// <summary>
+        ///   Gets or sets the color of the last joint of a model
+        /// </summary>
+        /// 
+        public Color EndJointColor { get; set; }
+
+        /// <summary>
+        ///   Gets or sets the color of the first joint of a model
+        /// </summary>
+        /// 
+        public Color BaseJointColor { get; set; }
+
+        /// <summary>
+        ///   Gets or sets the color of the rendering surface background
+        /// </summary>
+        /// 
+        public Color BackColor { get; set; }
+
+        /// <summary>
+        ///   Gets or sets the value to scale the drawing of the model to fit the window. Default is 1.
+        /// </summary>
+        /// 
+        public float Scale { get; set; }
+
+        /// <summary>
+        ///   Gets or sets the radius of the joints circles. Default is 8.
+        /// </summary>
+        /// 
+        public float JointRadius { get; set; }
+
+        /// <summary>
+        ///   Gets or sets the arrows indicating the axes on each drawing represented as a Rectangle object.
+        /// </summary>
+        /// 
+        public Rectangle ArrowsBoundingBox { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DenavitHartenbergViewer"/> class.
+        /// </summary>
+        /// <param name="width">
+        /// Width of the drawing window
+        /// </param>
+        /// <param name="height">
+        /// Height of the drawing window
+        /// </param>
+        public DenavitHartenbergViewer(int width, int height)
+        {
+            // Setting the default parameters
+            this.BackColor = Colors.Black;
+            this.LinksColor = Colors.Green;
+            this.JointsColor = Colors.Red;
+            this.BaseJointColor = Colors.Gold;
+            this.EndJointColor = Colors.Blue;
+            this.Scale = 1;
+            this.JointRadius = 8;
+
+            this.ArrowsBoundingBox = new Rectangle(10, height - 80, 50, 50);
+
+
+            // Creating the bitmap and graphics
+            this.xy = BitmapFactory.New(width, height);
+            this.yz = BitmapFactory.New(width, height);
+            this.xz = BitmapFactory.New(width, height);
+        }
+
+        /// <summary>
+        ///   Image of the model viewed on the XY plane.
+        /// </summary>
+        /// 
+        public BitmapSource PlaneXY
+        {
+            get { return this.xy; }
+        }
+
+        /// <summary>
+        ///   Image of the model viewed on the YZ plane.
+        /// </summary>
+        /// 
+        public BitmapSource PlaneYZ
+        {
+            get { return this.yz; }
+        }
+
+        /// <summary>
+        ///   Image of the model viewed on the XZ plane.
+        /// </summary>
+        /// 
+        public BitmapSource PlaneXZ
+        {
+            get { return this.xz; }
+        }
+
+        /// <summary>
+        /// Makes a list of all the models contained on a
+        ///   ModelCombinator. This function is recursive.
+        /// </summary>
+        /// <param name="model">
+        /// ModelCombinator model in which to extract all the models. 
+        /// </param>
+        /// <param name="models">
+        /// List of already extracted models. It accumulates all the 
+        ///   models at each call of this function.
+        /// </param>
+        /// <returns>
+        /// Returns a list of all the models contained in the 
+        /// ModelCombinator 'model' plus all previously extracted models
+        /// </returns>
+        private List<DenavitHartenbergModel> GetAllModels(DenavitHartenbergNode model, 
+            List<DenavitHartenbergModel> models)
+        {
+            // If it's the first call
+            if (models == null)
+            {
+                // Create the models list
+                models = new List<DenavitHartenbergModel>();
+            }
+
+            // Add the model contained in the ModelCombinator
+            models.Add(model.Model);
+
+            // For all the children of the ModelCombinator
+            foreach (DenavitHartenbergNode child in model.Children)
+            {
+                // Execute recursively this function 
+                models = this.GetAllModels(child, models);
+            }
+
+            // Return the models list
+            return models;
+        }
+
+        /// <summary>
+        /// Computes the three images of a list of ModelCombinator
+        /// </summary>
+        /// <param name="model">
+        /// List of arguments of models to be drawn
+        /// </param>
+        /// <remarks>
+        /// This function assumes that the models have already been calculated.
+        /// </remarks>
+        public void ComputeImages(params DenavitHartenbergNode[] model)
+        {
+            // List of models we will render
+            List<DenavitHartenbergModel> models_to_render = new List<DenavitHartenbergModel>();
+
+            // For each model on the argument list
+            for (int i = 0; i < model.Length; i++)
+            {
+                // Add the models extracted by the GetAllModels function to the list
+                models_to_render.AddRange(this.GetAllModels(model[i], null));
+            }
+
+            // Draw the extracted models
+            this.ComputeImages(models_to_render.ToArray());
+        }
+
+        /// <summary>
+        /// Computes the three images of a list of models
+        /// </summary>
+        /// <param name="model">
+        /// List of arguments of models
+        /// </param>
+        public void ComputeImages(params DenavitHartenbergModel[] model)
+        {
+            // Clear the 3 images with the background color
+            this.xy.Clear(this.BackColor);
+            this.xz.Clear(this.BackColor);
+            this.yz.Clear(this.BackColor);
+
+            // Draw each model
+            for (int i = 0; i < model.Length; i++)
+            {
+                DenavitHartenbergModel mdl = model[i];
+
+                // Draw each link of the model
+                Vector3 previous = mdl.Position;
+
+                for (var j = 0; j < mdl.Joints.Count; j++)
+                {
+                    Vector3 current = mdl.Joints[j].Position;
+
+                    // XY
+                    this.xy.DrawLine( 
+                        (int)Math.Round((this.xy.Width / 2) + previous.X * this.Scale), 
+                        (int)Math.Round((this.xy.Height / 2) - previous.Y * this.Scale), 
+                        (int)Math.Round((this.xy.Width / 2) + current.X * this.Scale), 
+                        (int)Math.Round((this.xy.Height / 2) - current.Y * this.Scale), 
+                        this.LinksColor);
+
+                    // ZY
+                    this.yz.DrawLine(
+                        (int)Math.Round((this.yz.Width / 2) + previous.Z * this.Scale), 
+                        (int)Math.Round((this.yz.Height / 2) - previous.Y * this.Scale), 
+                        (int)Math.Round((this.yz.Width / 2) + current.Z * this.Scale), 
+                        (int)Math.Round((this.yz.Height / 2) - current.Y * this.Scale), 
+                        this.LinksColor);
+
+                    // XZ
+                    this.xz.DrawLine(
+                        (int)Math.Round((this.xz.Width / 2) + previous.X * this.Scale), 
+                        (int)Math.Round((this.xz.Height / 2) - previous.Z * this.Scale), 
+                        (int)Math.Round((this.xz.Width / 2) + current.X * this.Scale), 
+                        (int)Math.Round((this.xz.Height / 2) - current.Z * this.Scale), 
+                        this.LinksColor);
+
+                    previous = current;
+                }
+
+                Color pJoints;
+
+                // Draw each joint
+                for (var j = 0; j < mdl.Joints.Count + 1; j++)
+                {
+                    Vector3 current;
+
+                    // Select the color of the joint
+                    if (j == 0)
+                    {
+                        pJoints = this.BaseJointColor;
+                        current = mdl.Position;
+                    }
+                    else
+                    {
+                        current = mdl.Joints[j - 1].Position;
+
+                        if (j == mdl.Joints.Count)
+                        {
+                            pJoints = this.EndJointColor;
+                        }
+                        else
+                        {
+                            pJoints = this.JointsColor;
+                        }
+                    }
+
+                    // XY
+                    this.xy.FillEllipseCentered(
+                        (int)Math.Round((this.xy.Width / 2) + current.X * this.Scale), 
+                        (int)Math.Round((this.xy.Height / 2) - current.Y * this.Scale), 
+                        (int)Math.Round(this.JointRadius), 
+                        (int)Math.Round(this.JointRadius), 
+                        pJoints);
+
+                    // YZ
+                    this.yz.FillEllipseCentered(
+                        (int)Math.Round((this.yz.Width / 2) + current.Z * this.Scale), 
+                        (int)Math.Round((this.yz.Height / 2) - current.Y * this.Scale), 
+                        (int)Math.Round(this.JointRadius), 
+                        (int)Math.Round(this.JointRadius), 
+                        pJoints);
+
+                    // XZ
+                    this.xz.FillEllipseCentered(
+                        (int)Math.Round((this.xz.Width / 2) + current.X * this.Scale), 
+                        (int)Math.Round((this.xz.Height / 2) - current.Z * this.Scale), 
+                        (int)Math.Round(this.JointRadius), 
+                        (int)Math.Round(this.JointRadius), 
+                        pJoints);
+                }
+            }
+
+            // Draw the arrows on the windows
+            this.DrawArrows(this.xy, "Y", "X");
+            this.DrawArrows(this.yz, "Y", "Z");
+            this.DrawArrows(this.xz, "Z", "X");
+
+
+        }
+
+        /// <summary>
+        /// Method to draw arrows to indicate the axis.
+        /// </summary>
+        /// <param name="g">
+        /// Graphics variable to use to draw.
+        /// </param>
+        /// <param name="topArrowText">
+        /// Text to draw on the top of the arrow.
+        /// </param>
+        /// <param name="rightArrowText">
+        /// Text to draw on the right arrow.
+        /// </param>
+        private void DrawArrows(WriteableBitmap g, string topArrowText, string rightArrowText)
+        {
+            var pAxes = Colors.White;
+            var bText = Colors.White;
+
+            // Draw the top arrow
+            WriteText(g, topArrowText, bText, this.ArrowsBoundingBox.Left - 5, this.ArrowsBoundingBox.Top - 15);
+
+            g.DrawLine(this.ArrowsBoundingBox.Left, this.ArrowsBoundingBox.Top, this.ArrowsBoundingBox.Left, this.ArrowsBoundingBox.Bottom, pAxes);
+
+            g.DrawLine(this.ArrowsBoundingBox.Left, this.ArrowsBoundingBox.Top, this.ArrowsBoundingBox.Left - 5, this.ArrowsBoundingBox.Top + 10, pAxes);
+
+            g.DrawLine(this.ArrowsBoundingBox.Left, this.ArrowsBoundingBox.Top, this.ArrowsBoundingBox.Left + 5, this.ArrowsBoundingBox.Top + 10, pAxes);
+
+            // Draw the right arrow
+            WriteText(g, rightArrowText, bText, this.ArrowsBoundingBox.Right - 10 + 12, this.ArrowsBoundingBox.Bottom - 6);
+
+            g.DrawLine(this.ArrowsBoundingBox.Left, this.ArrowsBoundingBox.Bottom, this.ArrowsBoundingBox.Right, this.ArrowsBoundingBox.Bottom, pAxes);
+
+            g.DrawLine(this.ArrowsBoundingBox.Right - 10, this.ArrowsBoundingBox.Bottom - 5, this.ArrowsBoundingBox.Right, this.ArrowsBoundingBox.Bottom, pAxes);
+
+            g.DrawLine(this.ArrowsBoundingBox.Right - 10, this.ArrowsBoundingBox.Bottom + 5, this.ArrowsBoundingBox.Right, this.ArrowsBoundingBox.Bottom, pAxes);
+        }
+
+        private static void WriteText(WriteableBitmap g, string text, Color color, int x, int y)
+        {
+            var formattedText = new FormattedText(
+                text,
+                CultureInfo.CurrentUICulture,
+                FlowDirection.LeftToRight,
+                new Typeface(new FontFamily("Arial"), FontStyles.Normal, FontWeights.Normal, FontStretches.Normal),
+                8.0,
+                new SolidColorBrush(color));
+
+            var drawingVisual = new DrawingVisual();
+            var drawingContext = drawingVisual.RenderOpen();
+            drawingContext.DrawText(formattedText, new Point(1, 1));
+            drawingContext.Close();
+
+            var render = new RenderTargetBitmap(20, 10, 96, 96, PixelFormats.Pbgra32);
+            render.Render(drawingVisual);
+            var renderBitmap = new WriteableBitmap(render);
+
+            g.Blit(new Rect(x, y, 20, 10), renderBitmap, new Rect(0, 0, 20, 10));
+        }
+    }
+}
---
+++ Kinematics.WPF/MainWindow.xaml.cs
@@ -0,0 +1,165 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="Cureos AB">
+//   Copyriht (c) 2015 Anders Gustafsson, Cureos AB
+// </copyright>
+// <summary>
+//   
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Kinematics.WPF
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Threading;
+
+    using Accord.Math.Kinematics;
+
+    using AForge.Math;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        #region FIELDS
+
+        /// <summary>
+        /// The arm base model
+        /// </summary>
+        private DenavitHartenbergModel model;
+
+        /// <summary>
+        /// The model left gripper
+        /// </summary>
+        private DenavitHartenbergModel model_tgripper;
+
+        /// <summary>
+        /// The model right gripper
+        /// </summary>
+        private DenavitHartenbergModel model_bgripper;
+
+        // The whole arm made of a combination of 
+        // the three previously declared models:
+        /// <summary>
+        /// </summary>
+        private DenavitHartenbergNode arm;
+
+        /// <summary>
+        /// The visualization model
+        /// </summary>
+        private DenavitHartenbergViewer viewer;
+
+        /// <summary>
+        /// Angle variable for animation
+        /// </summary>
+        private double angle = 0;
+
+        private DispatcherTimer timer1;
+
+        #endregion
+
+        /// <summary>
+        /// </summary>
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="sender">
+        /// </param>
+        /// <param name="e">
+        /// </param>
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            // Ok, let's start to build our virtual robot arm !
+            this.model = new DenavitHartenbergModel(new Vector3(0, 0, 0));
+
+            // Add the first joint 
+            this.model.Joints.Add(alpha: 0, theta: Math.PI / 4, radius: 35, offset: 0);
+
+            // Add the second joint
+            this.model.Joints.Add(alpha: 0, theta: -Math.PI / 3, radius: 35, offset: 0);
+
+            // Create the top finger
+            this.model_tgripper = new DenavitHartenbergModel();
+            this.model_tgripper.Joints.Add(alpha: 0, theta: Math.PI / 4, radius: 20, offset: 0);
+            this.model_tgripper.Joints.Add(alpha: 0, theta: -Math.PI / 3, radius: 20, offset: 0);
+
+            // Create the bottom finger
+            this.model_bgripper = new DenavitHartenbergModel();
+            this.model_bgripper.Joints.Add(alpha: 0, theta: -Math.PI / 4, radius: 20, offset: 0);
+            this.model_bgripper.Joints.Add(alpha: 0, theta: Math.PI / 3, radius: 20, offset: 0);
+
+            // Create the model combinator from the parent model
+            this.arm = new DenavitHartenbergNode(this.model);
+
+            // Add the top finger
+            this.arm.Children.Add(this.model_tgripper);
+
+            // Add the bottom finger
+            this.arm.Children.Add(this.model_bgripper);
+
+            // Calculate the whole model (parent model + children models)
+            this.arm.Compute();
+
+            // Create the model visualizer
+            this.viewer = new DenavitHartenbergViewer(300, 300) { JointRadius = 4f };
+
+            // Assign each projection image of the model to a picture box
+            this.pictureBox1.Source = this.viewer.PlaneXY;
+            this.pictureBox2.Source = this.viewer.PlaneXZ;
+            this.pictureBox3.Source = this.viewer.PlaneYZ;
+
+            // Start the animation
+            timer1 = new DispatcherTimer();
+            timer1.Interval = new TimeSpan(0, 0, 0, 0, 40);
+            timer1.Tick += this.timer1_Tick;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Let's move some joints to make a "hello" or "help meeee !" gesture !
+            model.Joints[0].Parameters.Theta = (float)(Math.Sin(angle) * Math.PI / 4 + Math.PI / 6);
+            model.Joints[1].Parameters.Theta = (float)(Math.Sin(angle) * Math.PI / 4 + Math.PI / 6);
+
+            // Increment the animation time
+            angle += (float)Math.PI / 30;
+
+            // Calculate the whole model
+            arm.Compute();
+
+            // Compute the images for displaying on the picture boxes
+            viewer.ComputeImages(arm);
+
+            // Refresh the pictures
+            pictureBox1.UpdateLayout();
+            pictureBox2.UpdateLayout();
+            pictureBox3.UpdateLayout();
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="sender">
+        /// </param>
+        /// <param name="e">
+        /// </param>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        private void PauseAndResumeButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (this.timer1.IsEnabled)
+            {
+                this.timer1.Stop();
+                this.PauseAndResumeButton.Content = "Resume";
+            }
+            else
+            {
+                this.timer1.Start();
+                this.PauseAndResumeButton.Content = "Pause";
+            }
+        }
+    }
+}
